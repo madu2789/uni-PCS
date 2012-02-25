8083c9040822ba57d3052348f5fbcc0d6b78c27f
metuskale@gmail.com <metuskale@gmail.com@e786e87d-ff6e-4379-8b97-06869fdaee6d>
2012-02-25 22:33:13 +0000

    Canviat el numero inicial de comensals a 1 Ja es poden eliminar elements del tool strip ; )

--- trunk/ProfitPlay/MainForm_Client/Client_Anula.vb
+++ trunk/ProfitPlay/MainForm_Client/Client_Anula.vb
@@ -1,5 +1,12 @@
 ﻿Public Class Client_Anula
 
+    Dim element As ToolStripItem
+    Private Sub addItem(ByVal cadena As String)
+
+        ToolStrip1.Items.Add(cadena)
+
+    End Sub
+
     Private Sub btn_estat_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_estat.Click
         'per fer!
     End Sub
@@ -19,4 +26,45 @@
         Me.Hide()
     End Sub
 
+    Private Sub btn_menu_anular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_menu_anular.Click
+
+    End Sub
+
+    Private Sub grb_anular_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles grb_anular.Enter
+
+    End Sub
+   
+    Private Sub Client_Anula_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim i As Integer = 0
+        Dim element_list As ToolStripItemCollection = ToolStrip1.Items
+        For i = 0 To element_list.Count
+
+            MsgBox("Nom: " & element_list.Item(i).Text, MsgBoxStyle.Critical)
+
+        Next
+    End Sub
+
+    Private Sub DeleteItem(ByVal sender As Object, ByVal e As System.Windows.Forms.ToolStripItemClickedEventArgs) Handles ToolStrip1.ItemClicked
+
+        Dim result = MsgBox("Realment vol eliminar " & e.ClickedItem.Text & "?", MsgBoxStyle.OkCancel, "Està segur?")
+        Dim element_list As ToolStripItemCollection = ToolStrip1.Items
+
+        If result = MsgBoxResult.Ok Then
+
+            For i As Integer = 0 To element_list.Count
+
+                If (element_list.Item(i).Text = e.ClickedItem.Text) Then
+
+                    element_list.RemoveAt(i)
+                    Exit For
+
+                End If
+
+            Next
+
+            ToolStrip1.Refresh()
+
+        End If
+
+    End Sub
 End Class
