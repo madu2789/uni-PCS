f08a019a2c39746e5968d04dff6d323edb0de2c5
metuskale <metuskale@gmail.com>
2012-03-06 09:14:35 +0000

    correccio de la ultima versio

---
+++ ProfitPlay/MainForm_Client/Comanda.vb
@@ -0,0 +1,37 @@
+﻿Public Class Comanda
+
+    Private productes As ArrayList
+
+    Public Sub New()
+
+        productes = New ArrayList
+
+    End Sub
+
+    Public Function getComanda() As ArrayList
+        Return productes
+    End Function
+
+    Public Sub setComanda(ByVal p As ArrayList)
+        Me.productes = p
+    End Sub
+
+    Public Sub deleteElement(ByVal p As String)
+
+        For i As Integer = 0 To productes.Count - 1 Step 1
+
+            If (productes.Item(i) = p) Then
+
+                productes.RemoveAt(i)
+
+            End If
+
+        Next
+
+    End Sub
+
+    Public Sub insertElement(ByVal s As String)
+        productes.Add(s)
+    End Sub
+
+End Class
