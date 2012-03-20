b3c72370e48958c559cbb3cdd755ab92b77902c5
madu2789 <madu2789@e786e87d-ff6e-4379-8b97-06869fdaee6d>
2012-03-20 13:54:44 +0000

    adaptador de dades passat al WS

--- trunk/ProfitPlay/MainForm_Client/Client_Carta.vb
+++ trunk/ProfitPlay/MainForm_Client/Client_Carta.vb
@@ -22,36 +22,13 @@
         m_comanda = New Comanda
 
         fillStructure()
-        ObteProductes()
+        'ObteProductes()
 
     End Sub
-
-    Private Sub ObteProductes()
-
-        Dim productesDB As DataTable
-
-        productesDB = ws.GetProducte
-        'prova
-        If productesDB Is Nothing Then
-            MsgBox("error", MsgBoxStyle.Critical)
-        End If
-
-        For Each fila In productesDB.Rows
-            'MsgBox(fila("password").ToString(), MsgBoxStyle.Exclamation)
-            'MsgBox(fila("rol").ToString(), MsgBoxStyle.Exclamation)
-
-            Dim p As Producto = New Producto()
-
-            p.setNom(fila("nombre").ToString)
-            p.setPreu(CDbl(fila("precio")))
-            p.setTipus(fila("categoria").ToString)
-            productes.Add(p)
-        Next
-
-    End Sub
-
 
     Private Sub fillStructure()
+
+
 
         For i As Integer = 0 To 10 Step 1
 
