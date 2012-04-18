2b3a5d590491c45dc5f6de38b3dcf10b095f5161
metuskale@gmail.com <metuskale@gmail.com@e786e87d-ff6e-4379-8b97-06869fdaee6d>
2012-04-18 10:27:55 +0000

    hem de mirar com fer lo del control de stock...

---
+++ trunk/ProfitPlay/MainForm_Client/Control_Stock.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Control_Stock
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_compra_stock = New System.Windows.Forms.Button
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox
+        Me.SuspendLayout()
+        '
+        'btn_compra_stock
+        '
+        Me.btn_compra_stock.Location = New System.Drawing.Point(157, 299)
+        Me.btn_compra_stock.Name = "btn_compra_stock"
+        Me.btn_compra_stock.Size = New System.Drawing.Size(75, 36)
+        Me.btn_compra_stock.TabIndex = 1
+        Me.btn_compra_stock.Text = "Comprar"
+        Me.btn_compra_stock.UseVisualStyleBackColor = True
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(120, 94)
+        Me.CheckedListBox1.TabIndex = 2
+        '
+        'Control_Stock
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(401, 345)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.btn_compra_stock)
+        Me.Name = "Control_Stock"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btn_compra_stock As System.Windows.Forms.Button
+    Friend WithEvents CheckedListBox1 As System.Windows.Forms.CheckedListBox
+End Class
---
+++ trunk/ProfitPlay/MainForm_Client/Control_Stock.vb
@@ -0,0 +1,15 @@
+﻿Public Class Control_Stock
+
+    Dim ws As New ws_profitplay.Service1SoapClient
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_compra_stock.Click
+
+    End Sub
+
+    Private Sub Control_Stock_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Dim plats = ws.GetAllPlats()
+        
+    End Sub
+End Class
