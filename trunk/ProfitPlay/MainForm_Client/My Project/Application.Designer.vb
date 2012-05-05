0e47a6714a0f0de8ec54bc93af23792a72b44134
metuskale@gmail.com <metuskale@gmail.com@e786e87d-ff6e-4379-8b97-06869fdaee6d>
2012-05-05 14:28:09 +0000

    implementada funcio per tal que es pugui obtenir el id del usuari apartir de la llista de taules es filtren correctament les comandes segons el id del usuari es sumen totes les comandes de un usuari per tenir el preu cal un camp a la BBDD que defineixi el numero de comensals

--- trunk/ProfitPlay/MainForm_Client/My Project/Application.Designer.vb
+++ trunk/ProfitPlay/MainForm_Client/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.MainForm_Client.Emp_carta
+            Me.MainForm = Global.MainForm_Client.Emp_Pago
         End Sub
     End Class
 End Namespace
